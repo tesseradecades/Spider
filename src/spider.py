c2ab5625620a92127292f2bd06fb9098ef62a324
tesseradecades <nwe8978@rit.edu>
2016-04-16 21:53:49 -0400

    Reads info from file for the necessary args

--- src/spider.py
+++ src/spider.py
@@ -5,22 +5,22 @@ import argparse
 COMMAND = None
 URL = None
 CUSTOM_AUTH = None
-COMMON_WORDS = None
-VECTORS = None
-SENSITIVE = None
-RANDOM = None
-SLOW = None
+COMMON_WORDS = []
+VECTORS = []
+SENSITIVE = []
+RANDOM = False
+SLOW = 500
 
 def main():
 	argParser = argparse.ArgumentParser()
 	argParser.add_argument("command",help="[discover | test]\n Discover - Output a comprehensive, human-readable list of all discovered inputs to the system. Techniques include both crawling and guessing.\n Test - Discover all inputs, then attempt a list of exploit vectors on those inputs. Report potential vulnerabilities.")
 	argParser.add_argument("url",help="Url to run the fuzzer on.")
 	argParser.add_argument("--custom-auth", help="--custom-auth=string Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa).")
-	argParser.add_argument("--common-words", help="--common-words=file Newline-delimited file of common words to be used in page guessing and input guessing.")
-	argParser.add_argument("--vectors", help="--vectors=file Newline-delimited file of common exploits to vulnerabilities.")
-	argParser.add_argument("--sensitive",help="--sensitive=file Newline-delimited file data that should never be leaked. It's assumed that this data is in the application's database (e.g. test data), but is not reported in any response.")
-	argParser.add_argument("--random",help="--random=[true|false] When off, try each input to each page systematically. When on, choose a random page, then a random input field and test all vectors. Default: false.")
-	argParser.add_argument("--slow",help="--slow=500 Number of milliseconds considered when a response is considered \"slow\". Default is 500 milliseconds")
+	argParser.add_argument("--common-words", type=argparse.FileType(mode='r'), help="--common-words=file Newline-delimited file of common words to be used in page guessing and input guessing.")
+	argParser.add_argument("--vectors", type=argparse.FileType(mode='r'), help="--vectors=file Newline-delimited file of common exploits to vulnerabilities.")
+	argParser.add_argument("--sensitive", type=argparse.FileType(mode='r'), help="--sensitive=file Newline-delimited file data that should never be leaked. It's assumed that this data is in the application's database (e.g. test data), but is not reported in any response.")
+	argParser.add_argument("--random", type=bool, help="--random=[true|false] When off, try each input to each page systematically. When on, choose a random page, then a random input field and test all vectors. Default: false.")
+	argParser.add_argument("--slow", type=int, help="--slow=500 Number of milliseconds considered when a response is considered \"slow\". Default is 500 milliseconds")
 	args = argParser.parse_args()
 	
 	global COMMAND
@@ -37,20 +37,34 @@ def main():
 	CUSTOM_AUTH = args.custom_auth
 	print(CUSTOM_AUTH)
 	
+	global COMMON_WORDS
+	if(args.common_words):
+		for word in args.common_words:
+			COMMON_WORDS.append(word.rstrip())
+	print(COMMON_WORDS)
+	
 	global VECTORS
-	VECTORS = args.vectors
+	if(args.vectors):
+		for word in args.vectors:
+			VECTORS.append(word.rstrip())
 	print(VECTORS)
 	
 	global SENSITIVE
-	SENSITIVE = args.sensitive
+	if(args.sensitive):
+		for word in args.sensitive:
+			SENSITIVE.append(word.rstrip())
 	print(SENSITIVE)
 	
 	global RANDOM
-	RANDOM = args.random
+	if(type(args.random) is str):
+		if(args.random.lower() == 'true'):
+			RANDOM = True
 	print(RANDOM)
 	
 	global SLOW
-	SLOW = args.slow
+	if(args.slow):
+		SLOW = args.slow
 	print(SLOW)
+
 if __name__ == "__main__":
 	main()
