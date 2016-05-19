d748d13edaea7da65d7c344ebcb4bf76d8207820
tesseradecades <nwe8978@rit.edu>
2016-05-19 15:04:07 -0400

    fully commented guess.py and test.py

--- src/guess.py
+++ src/guess.py
@@ -1,6 +1,16 @@
 __author__ = "Nathan Evans"
 
+"""
+A method to "fuzzy search" all html objects in a given list for the first one
+to nearly match any of the words in a given list of strings
 
+findMe - a list of strings to be used for fuzzy searched for
+
+inputs - a list of html objects to be searched through
+
+returns - the first html object to nearly match any word in findMe. If none of
+the objects in inputs nearly match, return None
+"""
 def findInput(findMe=[], inputs=[]):
 	for i in inputs:
 		if((i.get("name").lower() in findMe) | (i.get("class") in findMe) | (i.get("type") in findMe)):
--- src/test.py
+++ src/test.py
@@ -2,12 +2,35 @@ __author__="Nathan Evans"
 
 import random, requests, utility
 
+"""
+tests a given list of web pages for common vulnerabilities
+
+pages - a list of response objects representing web pages
+
+vectors - a list of input vulnerabilities to test for
+
+sensitive - a list of strings that should only be visible on the web page if a
+vector has successfully attacked the application
+
+rand - a boolean value to determine whether to randomly test a single input
+field on a single web page for a single vector (if True), or to test every
+field on every web page for every vector in vectors (if False)
+
+slow - the number of milliseconds that the spider should wait before a response
+should be considered too slow. Should the response time exceed this amount, the
+web page will be noted as having a potential denial of service vulnerability
+
+returns - a 3-D list representing the tested pages and the vulnerabilities they
+were found to have. ex) [[response, ["Potential Denial of Service", "'or'1'=='1'", ...]]]
+"""
 def testPages(pages=[], vectors=[], sensitive=[], rand=False, slow=500):
 	print("testPages")
 	if(len(pages) == 0):
 		print("no pages")
 		return pages
 	retList = []
+	
+	#get the necessary cookie in case the spider needs to be logged in
 	cookie = pages[len(pages)-1]
 	if(rand):
 		print("True rando")
@@ -40,12 +63,34 @@ def testPages(pages=[], vectors=[], sensitive=[], rand=False, slow=500):
 			successfulVectors.append("No successful attacks")
 		retList.append(successfulVectors)"""
 	else:
+		#a list conataining pages w/o the cookies
 		trueTestPages = pages[:len(pages)-1]
 		for p in trueTestPages:
 			retList.append(testPage(p, vectors, sensitive, slow, cookie))
 	print retList
 	return retList
 
+"""
+tests a single page for the vulnerabilities represented by a list of vectors
+
+tP - the page to be tested
+
+vectors - the list of vectors to test on the page
+
+sensitive - a list of strings that. None of these strings should appear on the
+test page unless one of the tested attack vectors is successful in attacking
+the application
+
+slow - the number of milliseconds that the spider should wait before a response
+should be considered too slow. Should the response time exceed this amount, the
+web page will be noted as having a potential denial of service vulnerability
+
+cook - the cookies the may be needed to remain logged into the system while
+testing
+
+returns - a 2-D list representation of the test page and any attack vectors
+that were successful. ex) [response, ["Potential Denial of Service", "'or'1'=='1',..."]]
+"""
 def testPage(tP, vectors=[], sensitive=[], slow=500.0, cook=''):
 	print("Test page:\t"+tP.url)
 	retList = [tP]
