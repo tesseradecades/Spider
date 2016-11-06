0b4521ac8a318262431c698cc45cd48cf6387518
tesseradecades <nathanjr95@gmail.com>
2016-11-06 15:35:01 -0500

    figured out how to get to page for entering crawl url

---
+++ app/controllers/crawl_controller.rb
@@ -0,0 +1,5 @@
+class CrawlController < ApplicationController
+	def new
+		render "new"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
   root 'welcome#index'
+
+  get 'new_crawl' => 'crawl#new', :as => "new_crawl"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
